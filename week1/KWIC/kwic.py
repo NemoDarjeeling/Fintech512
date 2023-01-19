30891ebe33c1f7854ade3c1b16831bd755ff5242
Haochen Yang <haochen.yang@duke.edu>
2023-01-19 11:30:05 -0500

    no input words to ignore or titles

--- week1/KWIC/kwic.py
+++ week1/KWIC/kwic.py
@@ -1,40 +1,51 @@
 import sys
 
-#read everything from stdin, until enter+ctrl+d used to signal the end; each line before "::" is one WTI, each line after "::" is one sentence to be sorted
-input_string = sys.stdin.read()
-a = input_string.split("::")
-ig_words = a[0].lower().strip('\n').split('\n')
-sentences = a[1].lower().strip('\n').split('\n')
-# ['descent of man', 'the ascent of man', 'the old man and the sea', 'a portrait of the artist as a young man', 'a man is a man but bubblesort is a dog']
-all_words = []
-for i in sentences:
-    word = i.split()
-    all_words.append(word)
+def main():
+    #read everything from stdin, until enter+ctrl+d used to signal the end; each line before "::" is one WTI, each line after "::" is one sentence to be sorted
+    input_string = sys.stdin.read()
+    a = input_string.split("::")
+    if len(a) != 2:
+        raise ValueError("please input words to ignore or titles")
+    ig_words = a[0].lower().strip('\n').split('\n')
 
-#get sorted unique sets of keywords that can be used to get corresponding sentence in all sentences 
-key_words = []
-for i in all_words:
-    for j in i:
-        if j not in ig_words:
-            key_words.append(j)
-key_words = sorted(list(set(key_words)))
+    sentences = a[1].lower().strip('\n').split('\n')
+    # ['descent of man', 'the ascent of man', 'the old man and the sea', 'a portrait of the artist as a young man', 'a man is a man but bubblesort is a dog']
+    all_words = []
+    for i in sentences:
+        word = i.split()
+        all_words.append(word)
 
-for i in key_words:
-    for j in sentences:
-        #track the starting index of keyword in each sentence, if multiple keywords, record multiple times
-        loc = []
-        m = j.find(i)
-        while m != -1:
-            loc.append(m)
-            m = j.find(i, m + len(i))
+    #get sorted unique sets of keywords that can be used to get corresponding sentence in all sentences 
+    key_words = []
+    for i in all_words:
+        for j in i:
+            if j not in ig_words:
+                key_words.append(j)
+    key_words = sorted(list(set(key_words)))
 
-        if loc:
-            #replace the lowercase keyword with uppercase keyword
-            for n in loc:
-                temp_dele = j[:n] + j[(n + len(i)):]
-                temp_inse = temp_dele[:n] + i.upper() + temp_dele[n:]
-                print(temp_inse)
+    result = ""
 
+    for i in key_words:
+        for j in sentences:
+            #track the starting index of keyword in each sentence, if multiple keywords, record multiple times
+            loc = []
+            m = j.find(i)
+            while m != -1:
+                loc.append(m)
+                m = j.find(i, m + len(i))
 
+            if loc:
+                #replace the lowercase keyword with uppercase keyword
+                for n in loc:
+                    temp_dele = j[:n] + j[(n + len(i)):]
+                    temp_inse = temp_dele[:n] + i.upper() + temp_dele[n:]
+                    print(temp_inse)
+                    result += temp_inse + "\n"
+    #remove the extra "\n" added
+    ulti_result = result[:-1] 
+    return ulti_result
+
+if __name__ == "__main__":
+    main()
 
 
--- week1/KWIC/test_kwic.py
+++ week1/KWIC/test_kwic.py
@@ -3,26 +3,30 @@ import sys
 import io
 import kwic
 
+#unittest has to work with a function that "returns" sth.
 class TestKeywords(unittest.TestCase):
-    def test_standard(self):
+    def test_std(self):
         # Test input
-        test_input = "the\na\nand\n::descent of man\nthe ascent of man\nthe old man and the sea\na portrait of the artist as a young man\na man is a man but bubblesort is a dog"
+        test_input = "is\nthe\nof\nand\nas\na\nbut\n::\nDescent of Man\nThe Ascent of Man\nThe Old Man and The Sea\nA Portrait of The Artist As a Young Man\nA Man is a Man but Bubblesort IS A DOG"
         # Expected output
         expected_output = "a portrait of the ARTIST as a young man\nthe ASCENT of man\na man is a man but BUBBLESORT is a dog\nDESCENT of man\na man is a man but bubblesort is a DOG\ndescent of MAN\nthe ascent of MAN\nthe old MAN and the sea\na portrait of the artist as a young MAN\na MAN is a man but bubblesort is a dog\na man is a MAN but bubblesort is a dog\nthe OLD man and the sea\na PORTRAIT of the artist as a young man\nthe old man and the SEA\na portrait of the artist as a YOUNG man"
 
         # redirect stdin
         sys.stdin = io.StringIO(test_input)
-        # capture stdout
-        sys.stdout = io.StringIO()
-        # run the code
-        kwic.main()
+        test_output = kwic.main()
 
-        # get stdout
-        output = sys.stdout.getvalue()
-        # compare stdout and expected output
-        self.assertEqual(output, expected_output)
+        self.assertEqual(test_output, expected_output)
+
+    def test_no_keywords(self):
+        with self.assertRaises(ValueError):
+            test_input = "Descent of Man\nThe Ascent of Man\nThe Old Man and The Sea\nA Portrait of The Artist As a Young Man\nA Man is a Man but Bubblesort IS A DOG"
+            sys.stdin = io.StringIO(test_input)
+            test_output = kwic.main()
+    
+
+
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity = 2)
 
 
