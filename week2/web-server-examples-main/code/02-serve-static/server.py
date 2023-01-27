d22ab29a0ecedec34a373959ac6ab7c7844b2a09
Haochen Yang <haochen.yang@duke.edu>
2023-01-27 11:48:05 -0500

    suspended program, no!

--- week2/web-server-examples-main/code/02-serve-static/server.py
+++ week2/web-server-examples-main/code/02-serve-static/server.py
@@ -26,10 +26,15 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
 
     # Classify and handle request.
     def do_GET(self):
+    #The method do_GET is called as part of the request handling process in the RequestHandler class. It is defined as a method on the http.server.BaseHTTPRequestHandler class, which RequestHandler inherits from. When a GET request is received by the server, this method is called to handle the request and generate a response.
         try:
 
             # Figure out what exactly is being requested.
             full_path = os.getcwd() + self.path
+# os.getcwd() is a function from the os module of the python standard library. It stands for "get current working directory" and it returns the current working directory(cwd) of the process as a string.
+# In the given code, it is used to concatenate the full path for a file that the client is requesting.
+# full_path = os.getcwd() + self.path
+# It's the current directory where the python script is running, so it will be the root directory of the server.
 
             # It doesn't exist...
             if not os.path.exists(full_path):
@@ -39,7 +44,7 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
             elif os.path.isfile(full_path):
                 self.handle_file(full_path)
 
-            # ...it's something we don't handle.
+            # ...it's something we don't handle. exists but not a file
             else:
                 raise ServerException("Unknown object '{0}'".format(self.path))
 
@@ -68,6 +73,8 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         self.send_header("Content-Length", str(len(content)))
         self.end_headers()
         #self.wfile.write(content.encode('utf-8'))
+        #important here, as the content of the error page is a string, which is a Unicode object. However, the write() method of the self.wfile object expects bytes, not a string. By encoding the string to UTF-8, you are converting it to a bytes object which can be sent over the network using the write() method.
+        #but if you want to display a html, use line below
         self.wfile.write(content)
 
 #-------------------------------------------------------------------------------
@@ -76,3 +83,7 @@ if __name__ == '__main__':
     serverAddress = ('', 8080)
     server = http.server.HTTPServer(serverAddress, RequestHandler)
     server.serve_forever()
+
+#why it only says ServerException: '/path_that_does_not_exist' not found, but there is no message of "Error accessing /path_that_does_not_exist"?
+#This is because the message in the handle_error method is not being displayed. The handle_error method is only constructing the error page with the message ServerException: '/path_that_does_not_exist' not found and the path requested, but it is not sending it to the browser.
+#You need to add self.wfile.write(content.encode('utf-8')) or self.wfile.write(content) to send the constructed error page to the browser.
