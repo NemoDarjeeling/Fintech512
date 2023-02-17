5d71fedfcf9313c3d718b9567e93c939a0f6af29
Haochen Yang <haochen.yang@duke.edu>
2023-02-17 15:31:10 -0500

    examples

---
+++ week5/examples/examples.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path='/static')
+#make it possible to run flask app by .py file name
+
+import math
+
+invoice = {
+    "customer": "BigCo",
+    "performances": [
+        {"playID": "Hamlet", "audience": 55},
+        {"playID": "As You Like It", "audience": 35},
+        {"playID": "Othello", "audience": 40}
+    ]
+}
+
+plays = {
+    "Hamlet": {"name": "Hamlet", "type": "tragedy"},
+    "As You Like It": {"name": "As You Like It", "type": "comedy"},
+    "Othello": {"name": "Othello", "type": "tragedy"}
+}
+# put all needed data here
+
+def format_as_dollars(amount):
+    return f"${amount:0,.2f}"
+# used for formatting
+
+def get_amount_for_performance(perf):
+    play = plays[perf['playID']]
+    if play['type'] == "tragedy":
+        this_amount = 40000
+        if perf['audience'] > 30:
+            this_amount += 1000 * (perf['audience'] - 30)
+    elif play['type'] == "comedy":
+        this_amount = 30000
+        if perf['audience'] > 20:
+            this_amount += 10000 + 500 * (perf['audience'] - 20)
+
+        this_amount += 300 * perf['audience']
+
+    else:
+        raise ValueError(f'unknown type: {play["type"]}')
+
+    return this_amount
+# used for calculating cost for each performance
+
+def get_total_amount():
+    total_amount = 0
+    for perf in invoice['performances']:
+        total_amount += get_amount_for_performance(perf)
+    return total_amount
+# add all cost for performances
+
+def get_volume_credits():
+    volume_credits = 0
+    for perf in invoice['performances']:
+        volume_credits += max(perf['audience'] - 30, 0)
+        if "comedy" == plays[perf['playID']]["type"]:
+            volume_credits += math.floor(perf['audience'] / 5)
+    return volume_credits
+# calculate volume credits
+
+@app.route('/statement')
+def statement():
+    return render_template('statement.html', customer = invoice["customer"], performances = invoice["performances"], plays = plays, format_as_dollars = format_as_dollars, get_amount_for_performance = get_amount_for_performance, get_total_amount=get_total_amount(), volume_credits=get_volume_credits())
+# render 
+
+@app.route('/stockchart')
+def stockchart():
+    return render_template('index.html')
---
+++ week5/examples/static/index.js
@@ -0,0 +1,106 @@
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+  }
+  
+  ready(() => { 
+      makeplot();
+  });
+
+function makeplot() {
+  document.getElementById("stock-select").addEventListener("change", function(){
+    fetch("static/data/" + this.value + ".csv")
+    .then((response) => response.text())
+    .catch(error => {
+        alert(error)
+    })
+    .then((text) => {
+      csv().fromString(text).then((data) => {
+        processData(data, this.value)
+      })
+    })
+  });  
+};
+
+
+function processData(data, stock) {
+  console.log("processData: start")
+  let x = [], y = []
+
+  for (let i=0; i<data.length; i++) {
+      row = data[i];
+      x.push( row['Date'] );
+      y.push( row['Close'] );
+  } 
+  makePlotly( x, y, stock );
+  console.log("processData: end")
+}
+
+function makePlotly( x, y, stock ){
+  console.log("makePlotly: start")
+  var traces = [{
+        x: x,
+        y: y
+  }];
+  var layout  = { title: stock}
+
+  myDiv = document.getElementById('myDiv');
+  Plotly.newPlot( myDiv, traces, layout );
+  console.log("makePlotly: end")
+};
+
+var dropdown = document.getElementById("stock-select");
+dropdown.addEventListener("change", function() {
+    var selectedValue = dropdown.value;
+    var url = `static/data/${selectedValue}.csv`;
+    fetch(url).then((response) => response.text()).then((text) => {
+        csv().fromString(text).then(processData);
+    })
+    .catch(error => {
+        alert(error);
+    });
+});
+
+function submit() {
+  bootbox.confirm({
+    title: "Submit?",
+    message: "Are you sure you want to submit now?",
+    buttons: {
+      cancel: {
+        label: '<i class="fa fa-times"></i> No'
+      },
+      confirm: {
+        label: '<i class="fa fa-check"></i> Yes'
+      }
+    },
+    callback: function(result) {
+      if (result) {
+        let dialog = bootbox.dialog({
+          title: 'Submitting Form',
+          message: '<p><i class="fas fa-spin fa-spinner"></i> Loading...</p>'
+        });
+        dialog.init(function() {
+          setTimeout(function() {
+              dialog.find('.bootbox-body').html('Form Submitted!');
+          }, 900);
+        });
+      }
+      else {
+        let dialog = bootbox.dialog({
+          title: 'Cancelling Action',
+          message: '<p><i class="fas fa-spin fa-spinner"></i> Loading...</p>'
+        });
+        dialog.init(function() {
+          setTimeout(function() {
+              dialog.find('.bootbox-body').html('Action Cancelled!');
+          }, 900);
+        });
+      }
+    }
+  });
+}
+
+
+
+
+
